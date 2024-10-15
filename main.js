27e40be74402736b84329105bd4287643ab10646
Jamie Brawn <jameshenrybrawn@gmail.com>
2024-10-15 19:02:38 +0100

    styles the grid

--- main.js
+++ main.js
@@ -1,21 +1,29 @@
 function draw() {
+  const size = 2;
 
-    const size = 2;
+  const canvas = document.getElementById("sudoku");
+  const ctx = canvas.getContext("2d");
 
-    const canvas = document.getElementById("sudoku");
-    const ctx = canvas.getContext("2d");
+  ctx.fillStyle = "black";
+  ctx.fillRect(0,0,410, 410)
 
-    for (var sx = 0; sx < size; sx++) {
-        for (var sy = 0; sy < size; sy++) {
-            for (var gx = 0; gx < size; gx++) {
-                for (var gy = 0; gy < size; gy++) {
+  for (var sx = 0; sx < size; sx++) {
+    for (var sy = 0; sy < size; sy++) {
+      var x = sx * size;
+      var y = sy * size;
 
-                    var x = sx * size + gx;
-                    var y = sy * size + gy;
+      ctx.fillStyle = "grey";
+      ctx.fillRect(x * 100 + 10, y * 100 + 10, 190, 190);
 
-                    ctx.fillRect((x * 100), (y * 100), 90, 90);
-                }
-            }
+      for (var gx = 0; gx < size; gx++) {
+        for (var gy = 0; gy < size; gy++) {
+          var x = sx * size + gx;
+          var y = sy * size + gy;
+
+          ctx.fillStyle = "white";
+          ctx.fillRect(x * 100 + 10 , y * 100 + 10, 90, 90);
         }
+      }
     }
+  }
 }
