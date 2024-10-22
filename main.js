22e0b0fe8a7f919404fd6e38b188663ad771206e
42 <33488710+FortyTwoFortyTwo@users.noreply.github.com>
2024-10-22 17:48:50 +0100

    Create array of symbols to display

--- main.js
+++ main.js
@@ -1,9 +1,28 @@
 function draw() {
-  const size = 2;
+  const size = 2;   // Can support up to 6
   const size_row = size * size; // Size for one of the whole row/col
   const size_square = 100;  // Size of the white squares
   const size_border = 10;   // Size of the border surrounding it
   const size_each = size_square + size_border;  // Size of white square and one side of border together
+
+  var symbols = []; // List of symbols to display
+
+  // Add in numbers
+  if (size == 6)    // 6 is 36, need all 1-9, A-Z and 0 to be able to have enough of it
+    symbols.push(0);
+
+  if (size != 5) {  // if size is 5, just go with alphabet for A-Y
+      const numbers_max = size_row < 9 ? size_row : 9;
+      for (var i = 0; i < numbers_max; i++)
+        symbols.push(i + 1);
+  }
+
+  // Add in alphabets
+  const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const alphabet_max = size_row - symbols.length;
+  for (var i = 0; i < alphabet_max; i++)
+    symbols.push(alphabet[i]);
+
 
   const canvas = document.getElementById("sudoku");
   const ctx = canvas.getContext("2d");
@@ -38,9 +57,13 @@ function draw() {
           ctx.font = size_square + "px sans-serif";
           ctx.textAlign = "center";
           ctx.textBaseline = "middle";
-          ctx.fillText("1", (x + 0.5) * size_each + size_border, (y + 0.5) * size_each + size_border);
+          ctx.fillText(getRandomItemInArray(symbols), (x + 0.5) * size_each + (size_border * 0.5), (y + 0.5) * size_each + size_border);
         }
       }
     }
   }
+}
+
+function getRandomItemInArray(array) {
+    return array[Math.floor(Math.random() * array.length)];
 }
